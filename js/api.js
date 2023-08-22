9c880cc1e9b9bca29387036417af274aad1007a2
kaiibe <kaiibe@mail.ru>
2023-08-22 20:11:58 +0200

    design changes

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-var apiKey = "";      // Paste you API key here
+var apiKey = "89a2173d9e7a8557d64eab9d8889aaea";      // Paste you API key here
 
 async function fetchWeatherData(latitude, longitude) {
   try {
@@ -14,24 +14,24 @@ async function fetchWeatherData(latitude, longitude) {
 }
 
 async function checkCity(inputCity, latitude, longitude) {
-    if (inputCity == null) {
-        const apiUrl = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${apiKey}`;
-        var response = await fetch(apiUrl);
-        var data = await response.json();
-        final = `${data[0].name}, ${data[0].country}`;
+  if (inputCity == null) {
+    const apiUrl = `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${apiKey}`;
+    var response = await fetch(apiUrl);
+    var data = await response.json();
+    final = `${data[0].name}, ${data[0].country}`;
 
-        return [data[0]["lat"], data[0]["lon"], final];
-    } else {
-        try {
-            var apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${inputCity}&limit=1&appid=${apiKey}`;
-            var response = await fetch(apiUrl);
-            var data = await response.json();
-            final = `${data[0].name}, ${data[0].country}`;
-        
-            return [data[0]["lat"], data[0]["lon"], final];
-          } catch (error) {
-            console.log("Error:", error);
-            return false;
-          }
+    return [data[0]["lat"], data[0]["lon"], final];
+  } else {
+    try {
+      var apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${inputCity}&limit=1&appid=${apiKey}`;
+      var response = await fetch(apiUrl);
+      var data = await response.json();
+      final = `${data[0].name}, ${data[0].country}`;
+
+      return [data[0]["lat"], data[0]["lon"], final];
+    } catch (error) {
+      console.log("Error:", error);
+      return false;
     }
+  }
 }
